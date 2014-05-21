cf33ed35bcc9b35ecdd071939a1bdfc2bf4a95c2
Debuff <Debuff@users.noreply.github.com>
2014-05-21 11:11:38 -0400

    Create MainActivity.fs

---
+++ FDroid/Beiginner/PopupMenuDemo/MainActivity.fs
@@ -0,0 +1,49 @@
+(*
+    F# Implementation of Xamarin Popup Menu Demo
+    Original C# Version: https://github.com/xamarin/monodroid-samples/tree/master/PopupMenuDemo
+    Port by: Francisco Munoz
+*)
+
+namespace PopupMenuDemo
+
+    open System
+
+    open Android.App
+    open Android.Content
+    open Android.OS
+    open Android.Runtime
+    open Android.Views
+    open Android.Widget
+    open Android.OS
+
+    // Our Main Activity
+    [<Activity (Label = "PopupMenuDemo", MainLauncher = true)>]
+    type MainActivity () =
+        inherit Activity ()
+
+        override this.OnCreate (bundle) =
+
+            base.OnCreate (bundle)
+
+            // Set our view from the "main" layout resource
+            this.SetContentView (Resource_Layout.Main)
+
+            let showPopupMenu = this.FindViewById<Button>(Resource_Id.popupButton)
+
+            showPopupMenu.Click.Add(fun args ->
+                let menu : PopupMenu = new PopupMenu (this, showPopupMenu)
+                
+                // with Android 3 need to use MenuInfater to inflate the menu
+                //menu.MenuInflater.Inflate (Resource.Menu.popup_menu, menu.Menu);
+
+                // with Android 4 Inflate can be called directly on the menu
+                menu.Inflate (Resource_Menu.popup_menu)
+                
+                menu.MenuItemClick.Add(fun args -> Console.WriteLine ("{0} Selected", args.Item.TitleFormatted) )
+                
+                // Android 4 now has the DismissEvent
+                menu.DismissEvent.Add(fun args -> Console.WriteLine ("Menu Dismissed") )
+                
+                menu.Show ()
+            )
+
